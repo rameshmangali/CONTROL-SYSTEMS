005d54d1129289608adae876a1df399ea3d6eff2
rameshmangali <mangaliramesh2004@gmail.com>
2024-07-19 09:35:27 +0530

    Add files via upload

---
+++ motor_position.m
@@ -0,0 +1,18 @@
+J = 3.2284E-6;
+b = 3.5077E-6;
+K = 0.0274;
+R = 4;
+L = 2.75E-6;
+s = tf('s');
+P_motor = K/(s*((J*s+b)*(L*s+R)+K^2))
+
+A = [0 1 0
+    0 -b/J K/J
+    0 -K/L -R/L];
+B = [0 ; 0 ; 1/L];
+C = [1 0 0];
+D = [0];
+
+motor_ss = ss(A,B,C,D)
+
+motor_ss = ss(P_motor)
